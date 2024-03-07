40bb1ddfc0fda6064abf3329bad3e3735f624871
Keisuke Hatasaki <hatasakick@gmail.com>
2024-03-07 12:20:24 +0000

    change library

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from flask import Flask, request, jsonify
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/add")
-def add_numbers(num1: int, num2: int):
-    return {"result": num1 + num2}
+@app.route('/add', methods=['GET'])
+def add():
+    # クエリパラメータから値を取得
+    num1 = request.args.get('num1', type=int)
+    num2 = request.args.get('num2', type=int)
+
+    # 加算結果を計算
+    result = num1 + num2
+
+    # 結果をJSONとして返す
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
